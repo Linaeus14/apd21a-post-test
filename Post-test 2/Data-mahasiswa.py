6ff7704f3f63f41e30c89b2ae775bb3c3e4f8928
Linaeus14 <titodqrmawan123@gmail.com>
2021-10-24 02:13:21 +0800

    part2

---
+++ Post-test 2/Data-mahasiswa.py
@@ -0,0 +1,21 @@
+"""
+Nama    : Tito Darmawan
+NIM     : 02109106042
+Prodi   : Informatika A
+"""
+#================================================
+print(
+    """
+            Data Mahasiswa
+ -------------------------------------
+Isilah data di bawah ini:
+    """
+)
+
+nam = str(input(
+ "Nama : "
+))
+
+nim = str(input(
+ "NIM : "
+))
